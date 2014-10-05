f5001b91ec10960412e0fe6bf8127cc3630becde
Abola Lee <abola921@gmail.com>
2014-10-05 23:47:11 +0800

    修正測試失敗內容

--- src/main/java/co/gibar/examples/jenkins/unittest/Holiday.java
+++ src/main/java/co/gibar/examples/jenkins/unittest/Holiday.java
@@ -2,7 +2,8 @@ package co.gibar.examples.jenkins.unittest;
 
 public class Holiday {
 
-	static String[] weekend = {"Fri", "Sun"};
+//	static String[] weekend = {"Fri", "Sun"};
+	static String[] weekend = {"Sat", "Sun"};
 	
 	public Boolean isHoliday(String today){
 		for(String holiday: weekend){  
