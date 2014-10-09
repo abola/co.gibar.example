7e1999ebd6396d39225140842c5075876d443ff1
Abola Lee <abola921@gmail.com>
2014-10-09 13:07:29 +0800

    Update JDK7OnlyScriptText.java
    多一個s

--- src/test/java/co/gibar/examples/jenkins/matrix/JDK7OnlyScriptText.java
+++ src/test/java/co/gibar/examples/jenkins/matrix/JDK7OnlyScriptText.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class JDK7OnlyScriptText {
 
   @Test public void testInstanceJDK7Script(){
-    Assert.asssertNotNull("new JDK7OnlyScript() 不應為null", new JDK7OnlyScript() );
+    Assert.assertNotNull("new JDK7OnlyScript() 不應為null", new JDK7OnlyScript() );
   }
   
 }
