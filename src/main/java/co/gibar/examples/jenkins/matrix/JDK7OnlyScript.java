bc388afcd2ec885f2a2e771fa4cda76dca4c3e1c
Abola Lee <abola921@gmail.com>
2014-10-09 13:32:32 +0800

    Update JDK7OnlyScript.java

--- src/main/java/co/gibar/examples/jenkins/matrix/JDK7OnlyScript.java
+++ src/main/java/co/gibar/examples/jenkins/matrix/JDK7OnlyScript.java
@@ -15,7 +15,9 @@ public class JDK7OnlyScript extends Thread {
 	public JDK7OnlyScript(){
 	    Thread jdk8stop = new JDK7OnlyScript("JDK8 stop test");
 	    jdk8stop.start();
-	    jdk8stop.join();
+	    try {
+	    	jdk8stop.join();
+	    } catch (InterruptedException e) {}	
 	}
 	
 	public JDK7OnlyScript(String name){
