800b598b749867eed06c6b3ad53dbe42be80595b
Abola Lee <abola921@gmail.com>
2014-10-09 13:09:56 +0800

    Update and rename JDK7OnlyScriptText.java to JDK7OnlyScriptTest.java
    Text > Test

--- src/test/java/co/gibar/examples/jenkins/matrix/JDK7OnlyScriptTest.java
+++ src/test/java/co/gibar/examples/jenkins/matrix/JDK7OnlyScriptTest.java
@@ -3,7 +3,7 @@ package co.gibar.examples.jenkins.matrix;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class JDK7OnlyScriptText {
+public class JDK7OnlyScriptTest {
 
   @Test public void testInstanceJDK7Script(){
     Assert.assertNotNull("new JDK7OnlyScript() 不應為null", new JDK7OnlyScript() );
